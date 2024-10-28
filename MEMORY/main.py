44af51fd80be72f16e32d3676173b761c1e7f0f6
hugooperezz <hugo.perez.redondo@colegiolitterator.com>
2024-10-28 22:54:51 +0100

    Tablero "boca abajo" completado

--- MEMORY/main.py
+++ MEMORY/main.py
@@ -1,28 +1,38 @@
-def jugar ():
+import random
+
+def jugar():
     print("¡Bienvenido al juego MEMORY!")
     print("Pon a prueba tu memoria encontrando todas las parejas de cartas.")
     print("Elige el tamaño del tablero y empieza a jugar.")
     print("¡Buena suerte y que comience la diversión!")
-    filas = int(input("Introduce las filas del tablero: "))
-    columnas = int(input("Introduce las columnas del tablero: "))
-    crear_tablero(filas,columnas)
+    solicitar_tamaño_tablero()
+
+def solicitar_tamaño_tablero():
+    while True:
+        filas = int(input("Ingrese el número de filas (entre 2 y 6): "))
+        columnas = int(input("Ingrese el número de columnas (entre 2 y 6): "))
+        if 2 <= filas <= 6 and 2 <= columnas <= 6:
+            if (filas * columnas) % 2 == 0:
+                crear_tablero(filas,columnas)
+            else:
+                print("El número total de posiciones debe ser par. Intente de nuevo.")
+        else:
+            print("Tamaño no válido. Elija un valor entre 2 y 6.")
 
 
-#Metodo para crear el tablero
-def crear_tablero (filas, columnas):
-    #Identifico que las medidas introducidas para crear el tablero son correctas
-    if filas * columnas % 2 != 0:
-        print("Error: no se puede realizar este tablero con las medidas introducidas")
-    else:
-        #Bucle for para crear el tablero
-        tablero = []
-        for _ in range(filas):
-            fila = ["*"] * columnas
-            tablero.append(fila)
+def crear_tablero(filas, columnas):
+    tablero = []
+    for _ in range(filas):
+        fila = []
+        for _ in range(columnas):
+            fila.append("X")
+        tablero.append(fila)
 
-#Metodo para mostrar el tablero
+    mostrar_tablero(tablero)
+
 def mostrar_tablero(tablero):
     for fila in tablero:
-        print("* " * len(fila))
+        print(" ".join(fila))
 
+# Inicia el juego
 jugar()
